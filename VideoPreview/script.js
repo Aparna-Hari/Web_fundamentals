4172eddcf0089b9a3d4f9986edc9f18d95905ae2
Aparna-Hari <101218385+Aparna-Hari@users.noreply.github.com>
2022-05-08 18:54:14 -0700

    Add files via upload

---
+++ VideoPreview/script.js
@@ -0,0 +1,18 @@
+console.log("page loaded...");
+
+
+function over(element) {
+    alert("mouseover");    
+}
+    
+function out(element) {
+    alert("mouseout");    
+}
+
+function play(element) {
+
+}
+
+function pause(element) {
+
+}
